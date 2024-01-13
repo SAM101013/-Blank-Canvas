e521a1bb5eb6fb8d06c94fb360a6d9018fb18e98
SAM101013 <amoonmarketing@proton.me>
2024-01-13 23:05:04 +0000

    A Blank Canvas

---
+++ scriptC.js
@@ -0,0 +1,8 @@
+// Get a reference to the canvas element
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
